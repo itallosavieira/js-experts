4d870c23edb1d0abf37fd755425eb7d8a5e4cfca
itallosavieira <itallo.sa.vieira@gmail.com>
2021-01-22 23:48:52 -0300

    set inicilizar() & efetuarParada no Proxy

---
+++ counter/contadorComponent.js
@@ -0,0 +1,67 @@
+const BTN_REINICIAR = "btnReiniciar"
+const ID_CONTADOR = "contador"
+const VALOR_CONTADOR = 100
+const PERIODO_INTERVALO = 10
+
+class ContadorComponent {
+
+    constructor() {
+        this.inicializar()
+    }
+
+    prepararContadorProxy() {
+        const handler = {
+            set: (currentContext, propertyKey, newValue) => {
+                console.log({ currentContext, propertyKey, newValue })
+                if(!currentContext.valor) {
+                    currentContext.efetuarParada()
+                }
+                currentContext[propertyKey] = newValue
+                return true
+            }
+        }
+
+        const contador = new Proxy({
+            valor: VALOR_CONTADOR,
+            efetuarParada: () => { }
+        }, handler)
+
+        return contador
+    }
+
+    atualizarTexto = ({ elementoContador, contador }) => () => {
+        const identificadorTexto = '$$contador'
+        const textoPadrao = `Começando em <strong>${identificadorTexto}</strong> segundos...`
+        elementoContador.innerHTML = textoPadrao.replace(identificadorTexto, contador.valor--)
+    }
+
+    agendarParadaContador({ elementoContador, idIntervalo}) {
+        return () => {
+            clearInterval(idIntervalo)
+
+            elementoContador.innerHTML = ""
+        }
+    }
+
+    inicializar() {
+        console.log('inicializou!')
+        const elementoContador = document.getElementById(ID_CONTADOR)
+
+        const contador = this.prepararContadorProxy()
+
+        const argumentos = {
+            elementoContador,
+            contador
+        }
+
+        const fn = this.atualizarTexto(argumentos)
+        const idIntervalo = setInterval(fn, PERIODO_INTERVALO)
+        
+        {
+            const argumentos = { elementoContador, idIntervalo }
+            const desabilitarBotao = () => console.log('desabilitou...')
+            const pararContadorFn = this.agendarParadaContador.apply({ desabilitarBotao}, [argumentos])
+            contador.efetuarParada = pararContadorFn
+        }
+    }
+}
